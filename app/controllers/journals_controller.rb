99e2c04eaac6904df5f84cc313e389ab20589362
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-08-26 12:18:19 -0700

    adds index and create actions to journals controller

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,2 +1,17 @@
 class JournalsController < ApplicationController
+  def index
+    journals = Journal.all
+    render json: journals.as_json
+  end
+
+  def create
+    journal = Journal.new (
+      name: params[:name]
+    )
+    if journal.save
+      render json: journal.as_json
+    else
+      render json: { errors: journal.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
