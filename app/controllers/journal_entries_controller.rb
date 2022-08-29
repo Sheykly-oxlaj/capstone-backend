0b44e91e480dd7c54cfca205e69ffded7fcae974
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-08-29 09:54:36 -0700

    fixes journal-entries controller

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -1,11 +1,13 @@
 class JournalEntriesController < ApplicationController
+  before_action :authenticate_user
+
   def index
-    journal_entries = JournalEntry.all
+    journal_entries = current_user.journal_entries
     render json: journal_entries.as_json
   end
 
   def show
-    journal_entry = JournalEntry.find_by(id: params[:id])
+    journal_entry = current_user.journal_entry.find_by(id: params[:id])
     render json: journal_entry.as_json
   end
 
@@ -13,6 +15,7 @@ class JournalEntriesController < ApplicationController
     journal_entry = JournalEntry.create(
       title: params[:title],
       text: params[:text],
+      journal_id: params[:text],
     )
     if journal_entry.save
       render json: journal_entry.as_json
