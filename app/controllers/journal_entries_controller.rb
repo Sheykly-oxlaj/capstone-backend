0f7deb097ebdd8fb84f2e1b3e79f26f41c1a68fe
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-08-29 11:19:12 -0700

    fixes show action to authenticate user

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -2,20 +2,24 @@ class JournalEntriesController < ApplicationController
   before_action :authenticate_user
 
   def index
-    journal_entries = current_user.journal_entries
+    journal_entries = JournalEntry.where(journal_id: params[:journal_id])
     render json: journal_entries.as_json
   end
 
   def show
-    journal_entry = current_user.journal_entry.find_by(id: params[:id])
-    render json: journal_entry.as_json
+    journal_entry = JournalEntry.find_by(id: params[:id])
+    if journal_entry.journal.user_id == current_user.id
+      render json: journal_entry.as_json
+    else
+      render json: { errors: "Must authenticate user" }, status: :unprocessable_entity
+    end
   end
 
   def create
-    journal_entry = JournalEntry.create(
+    journal_entry = JournalEntry.new(
       title: params[:title],
       text: params[:text],
-      journal_id: params[:text],
+      journal_id: params[:journal_id],
     )
     if journal_entry.save
       render json: journal_entry.as_json
