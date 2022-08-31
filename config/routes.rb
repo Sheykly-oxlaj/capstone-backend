ef28d9143c31370cb7f090f0f93b5cca12159a9b
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-08-31 13:26:39 -0700

    updates routes for journal-entries and adds description attribute to journals

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "/journals/:id" => "journals#show"
   post "/journals" => "journals#create"
 
-  get "/journal-entries/:id" => "journal_entries#show"
+  get "/journals/:id/journal-entries/:title" => "journal_entries#show"
   post "/journals/:id/journal-entries" => "journal_entries#create"
 
   post "/users" => "users#create"
---
+++ db/migrate/20220831202526_add_column_to_journals.rb
@@ -0,0 +1,5 @@
+class AddColumnToJournals < ActiveRecord::Migration[7.0]
+  def change
+    add_column :journals, :description, :text
+  end
+end
